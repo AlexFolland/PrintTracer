6e979f07b36cbd372075c4dc7c7e7df027c59be1
AlexFolland <lexlexlex@gmail.com>
2017-07-23 03:48:59 -0700

    handle multiple arguments

--- PrintTracer.lua
+++ PrintTracer.lua
@@ -1,6 +1,10 @@
 local oldprint = print
-print = function(text)
+print = function(text, ...)
+    for n=1,select('#', ...) do
+        local e = select(n, ...)
+        text = text.." "..tostring(e)
+    end
     local source = gsub(strtrim(debugstack(2,1,0),".\n"),"Interface\\AddOns\\","")
-    text = "PT: print(\""..tostring(text).."\") called from "..source
+    text = "PT: print(\""..text.."\") called from "..source
     return oldprint(text)
 end
